7e7ca3075b3ea471ffb0c7f5bf0afc6f8c2503da
Matteo De Pauw <matteodepauw@yahoo.com>
2022-11-18 08:47:30 +0100

    new file:   config.json 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/_app.js 	deleted:    pages/api/hello.js 	modified:   pages/index.js 	new file:   postcss.config.js 	deleted:    public/favicon.ico 	new file:   public/plt.png 	deleted:    public/vercel.svg 	new file:   services/LampService.js 	deleted:    styles/Home.module.css 	modified:   styles/globals.css 	new file:   tailwind.config.js

---
+++ services/LampService.js
@@ -0,0 +1,46 @@
+import config from "../config.json";
+import devData from "../../api/data.json";
+
+export async function getData() {
+    try {
+        const res = await fetch(config.api.url + config.api.handle.data)
+        return res.json();
+    } catch(err) {
+        console.log("API Request failed: " + err)
+        return devData;
+    }
+}
+
+export async function getDevData() {
+    return devData;
+}
+
+export async function getToggle() {
+    try {
+        const res = await fetch(config.api.url + config.api.handle.toggle);
+        return res.json();
+    } catch(err) {
+        console.log("API Request failed: " + err)
+    }
+}
+
+export async function getPlt() {
+    return;
+}
+
+export async function getDevPlt() {
+    return {
+        "src": "plt.png",
+        "day": 14,
+        "month": 11
+    }
+}
+
+export async function getState() {
+    try {
+        const res = await fetch(config.api.url + config.api.handle.state);
+        return res.json();
+    } catch(err) {
+        console.log("API Request failed: " + err)
+    }
+}
