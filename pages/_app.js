39755f8414101fd16040f698c0a34171f16bebb1
Matteo-DP <matteodepauw@yahoo.com>
2023-01-25 22:10:18 +0100

    fix(title)

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/globals.css'
+import Head from "next/head"
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return
+    <>
+      <Head>
+        <title>Lamp Dashboard | Matteo De Pauw</title>
+      </Head>
+      <Component {...pageProps} />
+    </> 
 }
