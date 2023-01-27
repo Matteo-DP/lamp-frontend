9028bd723df898dbf92185943078004f0dfd9425
Matteo-DP <matteodepauw@yahoo.com>
2023-01-27 17:47:19 +0100

    feat(ui): login page ui

--- pages/login.js
+++ pages/login.js
@@ -30,15 +30,30 @@ export default function Login() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            {error && <p>{error}</p>}
-            <p>Email:</p>
-            <input type="text" className='bg-zinc-100' ref={emailRef} />
-            <br />
-            <p>Password:</p>
-            <input type="password" className='bg-zinc-100' ref={passwordRef} />
-            <br />
-            <button type='submit' disabled={loading}>Login</button>
-        </form>
+        <main className='bg-bgdark w-screen h-screen flex justify-center items-center'>
+            <div className='bg-bgdark sm:border sm:border-bglight sm:shadow-2xl w-full max-w-sm px-8 py-8 rounded-xl'>
+                <form onSubmit={handleSubmit}>
+                    <h1 className='text-2xl text-accentpurple mb-8'>Please login to continue</h1>
+                    <p className='text-textlight mb-2'>
+                        Email:
+                    </p>
+                    <input type="email" className='mb-4 w-full px-4 py-2 rounded-sm' ref={emailRef} required placeholder='example@mail.com' />
+                    <br />
+                    <p className='text-textlight mb-2'>
+                        Password:
+                    </p>
+                    <input type="password" className='mb-4 w-full px-4 py-2 rounded-sm' ref={passwordRef} required placeholder="●●●●●"/>
+                    <br />
+                    <button type='submit' disabled={loading}
+                        className='px-4 py-2 bg-accentpurple rounded-xl text-textlight mt-8'
+                    >
+                        Login
+                    </button>
+                    {error && 
+                        <p className='text-accentred absolute'>{error}</p>
+                    }
+                </form>
+            </div>
+        </main>
   )
 }
